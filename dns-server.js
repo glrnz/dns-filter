0e9db3a6d763bd7ea1b8947ed8f5cf51cedfacb1
Gem Lorenz <gemlorenz10@gmail.com>
2016-12-08 21:12:26 +0800

    Unpopulate list
    set list to null after whitelist filled-up.

--- dns-server.js
+++ dns-server.js
@@ -1,27 +1,25 @@
 'use strict';
-/**
- * Configuration
- */
+// Configuration
 // Whitelist
 let TTL_FOR_BLOCK = 1800;
 let IP_FOR_BLOCK = '218.50.181.110';
 let authority = { address: '8.8.8.8', port: 53, type: 'udp' };
 let whitelistConf = 'whitelist.conf'; 
-//-------------------------- Read file for whitelist------------------//
+//-------------------------- Read file for whitelist------------------
 let fs = require('fs');
 let contents = fs.readFileSync(whitelistConf).toString();
-let list  = contents.split("\r\n");
+let list  = contents.split("\n");  //add \r so it will run smooth on windows
 let comment = /^#/;
 let whitelist = [];
 //populate whitelist array and remove comments
     list.forEach( ( val,index ) => {
         if ( val.match( comment ) || val == "" ){
-            //just dont push
+          //do nothing
         }else{
           whitelist.push( val );
         }
     });
-list = null; //unpopulate list, We already gave the datas to whitelist
+  list = null;
 // ------------------------ Do not edit below -------------------------------
 let dns = require('native-dns');
 let server = dns.createServer();
@@ -34,24 +32,16 @@ return date;
 let server_address;
 //Event Logging
   server.on('listening', () =>{
-    fs.appendFile('log/event.log', '\nServer started... ' + server.address().address + getDate() );
     console.log('Listening on... '+server.address().address + getDate());
   });
-  server.on('close', () => 
-    fs.appendFile('log/event.log','Server closed at:'+  getDate())
-  );
-  server.on('error', (err, buff, req, res) => 
-    fs.appendFile( 'log/event.log', '\nError at '+ getDate() +'Message:'+err.stack )
-  );    //console.error(err.stack));
-  server.on('socketError', (err, socket) =>  
-    fs.appendFile('log/event.log', '\nSocket Error at ' + getDate() + 'Message' +err) 
-  );    //console.error(err));
+  
+  server.on('error', (err, buff, req, res) => console.error(err.stack));    
+  server.on('socketError', (err, socket) => console.error(err));
 
 server.serve(53);
 //get current date and time for request logging.
 
 function proxy( question, response, cb ) {
-  //console.log('proxying', question.name);
   var request = dns.Request({
     question: question, // Forward the query
     server: authority,  // Parent DNS server to get IP address of the request
@@ -76,21 +66,14 @@ function handleRequest(request, response) {
   //
   request.question.forEach(question => {
       // Domains that you want allow.
-      //let entry = whitelist.filter( r => new RegExp(r, 'i').exec(question.name));
         let entry = whitelist.filter( ( currentValue, index, arr ) => {
                 if ( question.name.indexOf( currentValue ) != -1 ) return currentValue;
             });
           // When a domain matches in whitelist, response with real IP address.
         if (entry.length) {
-          // console.log('PASS: request from: ', request.address.address, ' for: ', question.name);
-             // fs.appendFile('log/passed.txt','\n'+ getDate() +' PASS: request from: ' + request.address.address + ' for: ' + question.name);
-             // fs.appendFile('log/data_log.txt','\n'+request.address.address + ' ' + question.name);
               f.push(cb => proxy(question, response, cb));
         }
         else { // or block it.
-          //  console.log('BLOCK: request from: ', request.address.address, ' for: ', question.name);
-             // fs.appendFile('log/blocked.txt','\n'+ getDate() +' BLOCK: request from: ' + request.address.address + ' for: ' + question.name);
-             // fs.appendFile('log/data_log.txt', '\n'+request.address.address + ' ' + question.name);
                 let record = {};
                 record.name = question.name;
                 record.ttl = TTL_FOR_BLOCK;
@@ -99,11 +82,7 @@ function handleRequest(request, response) {
                 response.answer.push(dns['A'](record));
         }
   });
-  // Do the proxying in parallel
-  // when done, respond to the request by sending the response
-  //
-  //SEND RESPONSE
-  //  
+
   async.parallel(f, function() { response.send()  ; });
 }
 server.on('request', handleRequest);
